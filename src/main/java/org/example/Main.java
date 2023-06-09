4b51f1a87d32bfddd60d3ac451dd262d4ddcb4fe
Akib Uz Zaman <akib.zaman.04@gmail.com>
2023-06-09 12:22:26 +0600

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * @author akib @Date 6/9/23
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
